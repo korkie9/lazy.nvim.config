5af2dee413daaac3a42e3a1d4bb3998211d418b2
Justin korkie <justin@Hiyori.hiyori.com>
2024-05-12 23:50:48 +0200

    feat(keymaps): add keymaps for terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
+-- Terminal
+vim.keymap.set("n", "<leader>tj", [[<Cmd>wincmd j<CR>]], { desc = "Move up from/to terminal" })
+vim.keymap.set("n", "<leader>tk", [[<Cmd>wincmd k<CR>]], { desc = "Move from from/to terminal" })
+vim.keymap.set("n", "<leader>tx", ":TermExec cmd='exit'<cr>", { desc = "Exit terminal" })
+
 -- Motions
 vim.keymap.set("n", "<C-j>", "10j", { silent = true })
 vim.keymap.set("n", "<C-k>", "10k", { silent = true })
